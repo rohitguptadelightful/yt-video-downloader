12a1d9f3326328ded8688337859510985426aada
rohitguptadelightfull <contact.teamrkg@gmail.com>
2022-09-23 19:12:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require("express")
+const ytdl = require("ytdl-core")
+
+const app = express()
+const PORT = process.env.port;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    return res.render("index");
+});
+
+app.get("/download", async (req, res) => {
+    const v_id = req.query.url.split('v=')[1];
+    const info = await ytdl.getInfo(req.query.url);
+    return res.render("download", {
+        url: "https://www.youtube.com/embed/" + v_id,
+        info: info.formats.sort((a, b) => {
+            return a.mimeType < b.mimeType;
+        }),
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+})
